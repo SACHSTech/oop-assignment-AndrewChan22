0651fcc46920cf4b2cc1c17937449cc512004ed2
AndrewChan22 <75390886+AndrewChan22@users.noreply.github.com>
2022-11-20 11:36:20 -0500

    Customer constructor

---
+++ src/online_store/Customer.java
@@ -0,0 +1,15 @@
+package online_store;
+
+public class Customer {
+    
+    private String fName;
+    private String lName;
+    private Address addr;
+
+    public Customer(String fName, String lName, Address addr) {
+        this.fName = fName;
+        this.lName = lName;
+        this.addr = addr;
+    }
+
+}
