29c6e9c79b4cf09aa200034c6a0daefbb9434eba
AndrewChan22 <75390886+AndrewChan22@users.noreply.github.com>
2022-11-20 09:34:10 -0500

    Instance variables + constructor for address

---
+++ src/Online Store/Address.java
@@ -0,0 +1,15 @@
+public class Address {
+
+    int houseNumber;
+    String streetName;
+    String city;
+    String province;
+    
+    public Address(int houseNumber, String streetName, String city, String province) {
+        this.houseNumber = houseNumber;
+        this.streetName = streetName;
+        this.city = city;
+        this.province = province;
+    }
+
+}
