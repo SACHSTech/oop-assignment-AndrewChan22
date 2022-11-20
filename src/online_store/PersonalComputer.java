887d114f5f96bb233b018cf328e711a865af3e59
AndrewChan22 <75390886+AndrewChan22@users.noreply.github.com>
2022-11-20 11:13:55 -0500

    PC class, instance variables, methods, constructor

---
+++ src/online_store/PersonalComputer.java
@@ -0,0 +1,38 @@
+package online_store;
+
+public class PersonalComputer extends Products {
+    private String processor;
+    private double storageSSD;
+    private double storageRAM;
+
+    public PersonalComputer(double price, String productName, int amount, String processor, double storageSSD, double storageRAM) {
+        super(price, productName, amount);
+        this.storageSSD = storageSSD;
+        this.storageRAM = storageRAM;
+    }
+
+    public String getProcessor() {
+        return processor;
+    }
+
+    public double getSSD() {
+        return storageSSD;
+    }
+
+    public double getRAM() {
+        return storageRAM;
+    }
+
+    public void getFeatures() {
+        System.out.println("Processor: ");
+        if (storageSSD >= 1000) {
+            System.out.println(storageSSD + "TB solid-state drive (SSD)");
+        }
+        else {
+            System.out.println(storageSSD + "GB solid-state drive (SSD)");
+        }
+
+        System.out.println("RAM: " + storageRAM + "GB");
+    }
+
+}
