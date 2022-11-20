a7121619841bb7c1071285ccbabd43168dbfd62f
AndrewChan22 <75390886+AndrewChan22@users.noreply.github.com>
2022-11-20 11:50:19 -0500

    ShoppingCart class

---
+++ src/online_store/ShoppingCart.java
@@ -0,0 +1,24 @@
+package online_store;
+
+import java.util.ArrayList;
+public class ShoppingCart {
+    private int totalItems;
+    private double totalCost;
+    private ArrayList<Products> products = new ArrayList<Products>();
+
+    public ShoppingCart (int totalItems, double totalCost, ArrayList<Products> products) {
+        this.totalItems = totalItems;
+        this.totalCost = totalCost;
+        this.products = products;
+    }
+
+    public int getTotalItems() {
+        return totalItems;
+    }
+
+    public double getTotalCost() {
+        return totalCost;
+    }
+
+    
+}
