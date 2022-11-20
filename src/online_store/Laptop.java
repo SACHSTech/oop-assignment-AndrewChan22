a352e9a7d3be1eb37d6b96b3df4340cf2fd35883
AndrewChan22 <75390886+AndrewChan22@users.noreply.github.com>
2022-11-20 11:26:16 -0500

    Laptop constructor

---
+++ src/online_store/Laptop.java
@@ -0,0 +1,17 @@
+package online_store;
+
+public class Laptop extends PersonalComputer {
+
+    private boolean hasWebcam;
+    private boolean isTouchscreen;
+    private double weight;
+
+    public Laptop (double price, String productName, int amount, String processor, double storageSSD, double storageRAM, boolean hasWebcam, boolean isTouchscreen, double weight) {
+        super(price, productName, amount, processor, storageSSD, storageRAM);
+        this.hasWebcam = hasWebcam;
+        this.isTouchscreen = isTouchscreen;
+        this.weight = weight;
+    }
+    
+    
+}
