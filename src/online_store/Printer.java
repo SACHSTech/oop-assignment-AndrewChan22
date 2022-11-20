8f2b60d0d310ad63ad1648523804bc5f63bc8201
AndrewChan22 <75390886+AndrewChan22@users.noreply.github.com>
2022-11-20 11:31:38 -0500

    Printer instance variable and methods

---
+++ src/online_store/Printer.java
@@ -0,0 +1,18 @@
+package online_store;
+
+public class Printer extends Products {
+    private String type;
+
+    public Printer(double price, String productName, int amount) {
+        super(price, productName, amount);
+        this.type = type;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void getFeatures() {
+        System.out.println("This printer is a " + type);
+    }
+}
